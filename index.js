31adc31887745105bc00900b41719ff8acc66be0
ElmTheDev <elmthedev@gmail.com>
2017-08-06 01:58:14 +0200

    Formatting and some minor things

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// Author: Elm (elmthedev@gmail.com)
+// Author: Elm (elmthedev@gmail.com) (https://github.com/ElmTheDev/Youtube-Bulk-MP3-Downloader)
 
 // Configuration
-
-let apiKey     = ""; // YouTube V3 API Key
-let songList   = "./songs.txt"; // Path to song list
+let apiKey = ""; // YouTube V3 API Key
+let songList = "./songs.txt"; // Path to song list
 let fileFormat = "mp3"; // File format you want video to be outputted to
 let ffmpegPath = "./ffmpeg/ffmpeg.exe"; // Path to ffmpeg.exe
 let outputPath = "./mp3"; // Folder where video will be outputted
@@ -11,27 +10,28 @@ let outputPath = "./mp3"; // Folder where video will be outputted
 // Don't edit under this line if you have no idea what you are doing.
 
 const YoutubeMp3Downloader = require("youtube-mp3-downloader");
-const YouTube    		   = require("youtube-node");
-const fs 		           = require("fs");
+const YouTube = require("youtube-node");
+const fs = require("fs");
 
-let youTube 			   = new YouTube();
-let currentVideoId         = 1;
-let songQueue              = [];
+let youTube = new YouTube();
+let currentVideoId = 1;
+let songQueue = [];
 
 youTube.setKey(apiKey);
 
- 
+
 let YD = new YoutubeMp3Downloader({
     "ffmpegPath": ffmpegPath,
     "outputPath": outputPath,
     "youtubeVideoQuality": "highest",
     "queueParallelism": 2,
-    "progressTimeout": 2000      
+    "progressTimeout": 2000
 });
- 
-fs.readFile("./"+songList, function(err, data) {
+
+fs.readFile("./" + songList, function(err, data) {
     if (err) throw err;
     songQueue = data.toString().replace(/\r/g, "").split("\n");
+    songQueue.push("");
 
     SearchYoutube(songQueue[0], function(returnValue) {
         DownloadNextOne(returnValue);
@@ -48,24 +48,23 @@ YD.on("finished", function(err, data) {
 });
 
 YD.on("error", function(error) {
-	console.log(`Ouch! There was an error!\n\n${error}`);
-	process.exit(0);
+    console.log(`Ouch! There was an error!\n\n${error}`);
+    process.exit(0);
 });
 
 YD.on("progress", function(progress) {
     let progressNice = (progress.progress.percentage).toFixed(2);
     let speedNice = (progress.progress.speed / 1000).toFixed(2);
-	let songName = songQueue[currentVideoId - 1];
-	console.log(`Video: '${songName}' | Progress: ${progressNice}% | Speed: ${speedNice}kb/s | Currently downloading ${currentVideoId}/${songQueue.length}`);
+    let songName = songQueue[currentVideoId - 1];
+    console.log(`Video: '${songName}' | Progress: ${progressNice}% | Speed: ${speedNice}kb/s | Currently downloading ${currentVideoId}/${songQueue.length-1}`);
 });
 
 function DownloadNextOne(videoId) {
-	if(currentVideoId == songQueue.length)
-	{
-		console.log("\n\n\nFinished downloading music!");
-		process.exit(0);
-	}
-    YD.download(videoId, mysqlEscape(songQueue[currentVideoId-1]) + "." + fileFormat);
+    if (currentVideoId == songQueue.length) {
+        console.log("\n\n\nFinished downloading music!");
+        process.exit(0);
+    }
+    YD.download(videoId, mysqlEscape(songQueue[currentVideoId - 1]) + "." + fileFormat);
 }
 
 function SearchYoutube(string, callback) {
@@ -80,8 +79,8 @@ function SearchYoutube(string, callback) {
     });
 }
 
-function mysqlEscape(stringToEscape){
-    if(stringToEscape == '') {
+function mysqlEscape(stringToEscape) {
+    if (stringToEscape == '') {
         return stringToEscape;
     }
 
